6e45865a5fce44f50186b3a07a0dd9fea0109ac9
Kdoggg666 <kenanwright@gmail.com>
2022-03-17 09:24:43 +0000

    Add usestates for user objects

--- practice_app/src/App.js
+++ practice_app/src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Tweet from './Tweet';
 
 
 function App() {
+
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const incriment = () => {
+    setCount(count + 1);
+    setRed(!isRed)
+  };
+
+  const [users, setUsers] = useState([
+    {name: 'john', message: 'Hello', likes: 125},
+    {name: 'dave', message: 'howdy', likes: 1255},
+    {name: 'pete', message: 'sup?', likes: 185}
+  ]);
+
   return(
     <div className='app'>
-      <Tweet name="Dave" message="Random text 1" likes="125"/>
-      <Tweet name="Steve" message="Random text 2" likes="1255"/>
-      <Tweet name="John" message="Random text 3" likes="12555"/>
-      <Tweet name="Boris" message="Random text 4" likes="12554556"/>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message} likes={user.likes}/>
+      ))}
+      <h1 className={isRed ? 'red' : ''}>Change my color!</h1>
+      <button onClick={incriment}>Incriment</button>
+      <h1>{count}</h1>
     </div>
   );
 }
